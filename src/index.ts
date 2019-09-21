93a1a1af214828b30260f9709911e69fd673bcac
Sandy Lu <chaiyinglu@gmail.com>
2019-09-21 15:39:46 +0200

    add socket io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import Bundler from 'parcel-bundler';
 import path from 'path';
+import http from 'http';
+import SocketIOServer from 'socket.io';
+import initializeSocketIO from './socket';
 
 const app = express();
+const server = new http.Server(app);
+const io = SocketIOServer(server);
 const port = 8080 || process.env.PORT;
-
 const bundler = new Bundler(path.join(__dirname, '../src/client/index.html'));
+
+initializeSocketIO(io);
 app.use(bundler.middleware());
 
-app.listen(port, () => {
+server.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`);
 });
